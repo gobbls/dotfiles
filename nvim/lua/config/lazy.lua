57e498f9405e5e7e6d2d014a1a49bec33f58742b
gobbls <98641699+gobbls@users.noreply.github.com>
2025-09-12 21:03:20 +0200

    Make lazy not look for updates on launch.

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -4,13 +4,13 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 	if vim.v.shell_error ~= 0 then
-	vim.api.nvim_echo({
-		{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-		{ out, "WarningMsg" },
-		{ "\nPress any key to exit..." },
-	}, true, {})
-	vim.fn.getchar()
-	os.exit(1)
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
 	end
 end
 vim.opt.rtp:prepend(lazypath)
@@ -25,5 +25,5 @@ require("lazy").setup({
 	-- colorscheme that will be used when installing plugins.
 	install = { colorscheme = { "kanagawa" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+	checker = { enabled = false },
 })

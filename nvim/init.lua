661f5f7bf6968f28d6baa06fd7adc6ee22c693a8
gobbls <98641699+gobbls@users.noreply.github.com>
2025-09-19 23:35:29 +0200

    Some autocmd for ruff, recomended by ruff.
    For better integration with Pyright.

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,9 +14,26 @@ vim.opt.cursorline = true
 vim.opt.signcolumn = "yes"
 
 -- Ignore Neovim providers
-vim.g.loaded_python3_provider = 0
+--vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
+
+-- Defer hoverProvider to pyright instead of ruff.
+-- https://docs.astral.sh/ruff/editors/setup/#neovim
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client == nil then
+			return
+		end
+		if client.name == "ruff" then
+			-- Disable hover in favor of Pyright
+			client.server_capabilities.hoverProvider = false
+		end
+	end,
+	desc = "LSP: Disable hover capability from Ruff",
+})
 
 -- Fetch the lazy configs
 require("config.keymaps")

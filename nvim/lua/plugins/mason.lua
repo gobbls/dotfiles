8463e8c2c80681d0cd1aa7b16039726ef4fbcabd
gobbls <98641699+gobbls@users.noreply.github.com>
2025-09-19 23:31:08 +0200

    Fixed auto-installation of Mason packages.
    Finally installs automatically on first setup, but still requires
    external dependencies (lua5.1, lazygit, npm-treesitter etc).
    Easy to just checkhealth and install the missing dependencies, Mason
    will scream for them....

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,18 +1,27 @@
 return {
 	"mason-org/mason.nvim",
-	opts = {
-		-- Figure out how to install these if missing...
-		-- Currently no way for Mason to do so without another plugin.
-		-- "shfmt",
-		-- "css-lsp",
-		-- "html-lsp",
-		-- "json-lsp",
-		-- "lua-language-server",
-		-- "marksman",
-		-- "omnisharp",
-		-- "ruff",
-		-- "stylua",
-		-- "svelte-language-server",
-		-- "typescript-language-server",
+	dependencies = {
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			automatic_enable = false,
+			ensure_installed = {
+				"bash-language-server",
+				"css-lsp",
+				"html-lsp",
+				"lua-language-server",
+				"marksman", -- markdown linting
+				"omnisharp",
+				"prettier",
+				"pyright", -- for linting
+				"ruff", -- for formatting
+				"shfmt", -- formatting
+				"stylua", -- formatting
+				"svelte-language-server",
+				"typescript-language-server",
+
+				--"json-lsp", -- maybe don't need this
+			},
+		},
 	},
+	opts = {},
 }

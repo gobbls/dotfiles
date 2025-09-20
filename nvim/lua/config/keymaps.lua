44382e1a0d68e83457b9dbcef8ac1a7bbadf4190
gobbls <98641699+gobbls@users.noreply.github.com>
2025-09-20 22:08:57 +0200

    Better keymaps.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,13 +1,15 @@
--- Leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Up, Down
 -- Better up/down stolen from LazyVim: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 vim.keymap.set({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 vim.keymap.set({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
+vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle, { desc = "NeovimTree" })
+vim.keymap.set("n", "<leader>d", "<CMD> lua vim.diagnostic.open_float() <CR>", { desc = "Open wrapped lint-message." })
 
 vim.keymap.set("n", "<leader>cl", function()
 	local col = vim.api.nvim_get_option_value("colorcolumn", {})
@@ -18,14 +20,5 @@ vim.keymap.set("n", "<leader>cl", function()
 	end
 end, { desc = "Toggle colored bar at column 80." })
 
--- Open lint-message in floating window
-vim.keymap.set("n", "<leader>dd", "<CMD> lua vim.diagnostic.open_float() <CR>", { desc = "Open wrapped lint-message." })
-
--- Exit editor
-vim.keymap.set("n", "<leader>qq", vim.cmd.q)
-
 -- Open Netrw
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
--- Open NvimTree
-vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle, { desc = "NeovimTree" })

9476a1f056e3cc4d341a93d96b31b9f9f0508b4c
gobbls <98641699+gobbls@users.noreply.github.com>
2025-09-21 21:02:44 +0200

    Removed some comments.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,7 @@
--- disable Netrw as the file-explorer
+-- disable Netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- Editor preference
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.number = true
@@ -10,16 +9,13 @@ vim.opt.relativenumber = true
 vim.opt.colorcolumn = "80"
 vim.opt.cursorline = true
 vim.opt.guicursor = "n-v-c-i:block"
-
--- Preset a column in the gutter for linting symbols.
 vim.opt.signcolumn = "yes"
 
--- Ignore Neovim providers
---vim.g.loaded_python3_provider = 0
+-- Ignore providers
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
--- Defer hoverProvider to pyright instead of ruff.
+-- Disable ruff hover in favor of Pyright
 -- https://docs.astral.sh/ruff/editors/setup/#neovim
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
@@ -29,13 +25,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
 			return
 		end
 		if client.name == "ruff" then
-			-- Disable hover in favor of Pyright
 			client.server_capabilities.hoverProvider = false
 		end
 	end,
 	desc = "LSP: Disable hover capability from Ruff",
 })
 
--- Fetch the lazy configs
 require("config.keymaps")
 require("config.lazy")

6129d9ad6904de7a9155321374ba16f7acaa10f7
gobbls <98641699+gobbls@users.noreply.github.com>
2026-09-02 04:22:56 +0200

    neovim transparent background and bit prettier tabline

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,7 +26,9 @@ vim.g.loaded_ruby_provider    = 0
 
 -- Set built-in colorscheme.
 vim.cmd("colorscheme habamax")
-vim.api.nvim_set_hl(0, "Normal", { bg = "#111111" })
+--vim.api.nvim_set_hl(0, "Normal", { bg = "#111111" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TablineSel", {fg = "orange" })
 
 --------------------
 -- Config modules --
